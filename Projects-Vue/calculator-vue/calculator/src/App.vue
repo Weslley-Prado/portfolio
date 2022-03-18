89c920af328bf9ca54eeb7abb5b8af28e2598faf
Weslley Prado <weslley.prado73@gmail.com>
2022-03-18 08:53:18 -0300

    new projects

---
+++ Projects-Vue/calculator-vue/calculator/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <CalculatorWeb />
+</template>
+
+<script>
+import CalculatorWeb from './components/CalculatorWeb.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorWeb
+  }
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "Roboto";
+    src: url("./fonts/Roboto-Thin.ttf");
+}
+* {
+    font-family: "Roboto", monospace;
+}
+body {
+    margin: 0;
+}
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
