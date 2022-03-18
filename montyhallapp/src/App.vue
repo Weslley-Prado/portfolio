39a12941163e5ae030b835858c18fe34c7fd4d0a
Weslley Prado <weslley.prado73@gmail.com>
2022-03-18 13:05:22 -0300

    Finish

--- montyhallapp/src/App.vue
+++ montyhallapp/src/App.vue
@@ -1,19 +1,44 @@
 <template>
 <div>
   <HelloWorld msg="Problema de Monty Hall"/>
-  <GiftHall />
+  <div class="form">
+            <div v-if="!started">
+                <label for="portsAmount">Quantas portas? </label>
+                <input type="text" id="portsAmount" size="3"
+                    v-model.number="portsAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedPort">Qual porta é premiada? </label>
+                <input type="text" id="selectedPort" size="3"
+                    v-model.number="selectedPort">
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in portsAmount" :key="i">
+                <DoorHall :hasGift="i === selectedPort" :number="i" />
+            </div>
+        </div>
 </div>  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import GiftHall from './components/Gift.vue'
+import DoorHall from './components/Door.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    GiftHall
+    DoorHall
+  },
+  data: function() {
+        return {
+            started: false,
+            portsAmount: 3,
+            selectedPort: null
+        }
   }
 }
 </script>
