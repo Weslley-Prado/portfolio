3f36e88e1e96ec334007cf39e46095d96f97bb96
Weslley Prado <weslley.prado73@gmail.com>
2022-03-20 14:58:13 -0300

    Tela de login

--- Projects React Native/replica_coursify.me/coursifyme/App.js
+++ Projects React Native/replica_coursify.me/coursifyme/App.js
@@ -1,13 +1,56 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Icon, Input, Text } from "react-native-elements";
+import   Icon from "react-native-vector-icons/FontAwesome";
 
-export default function App(){
-  return(
-    <View style={styles.container}>
-      <Text> Entre no Cousifyme</Text>
-    </View>
-  )
+
+class App extends Component{
+  constructor(props){
+    super(props);
+     const [email, setEmail] = useState(null)
+     const [password, setPassword] = useState(null)
+    
+    };
+    render(){
+    return(
+     <View style={styles.container}>
+      <Text h3> Entre no Cousifyme</Text>
+      <Input 
+      placeholder="E-mail"
+      leftIcon={{type: 'font-aesome', name: 'envelope'}}
+      onChangeText={value => setEmail(value)}
+      keyboardType="email-address"
+      />
+       <Input 
+      placeholder="Password"
+      leftIcon={{type: 'font-aesome', name: 'lock'}}
+      onChangeText={value => setPassword(value)}
+      secureTextEntry={true} 
+      />
+< Button 
+icon={
+  <Icon
+   name="arrow-right"
+   size={15}
+   color = 'white'
+   />
 }
+title="Entrar"
+
+/>
+
+
+      />
+    </View>  
+    );
+  }
+
+}
+    
+
+
+
+
 
 const syles = StyleSheet.create({
   container: {
