03501468325d3504bb205a479e13c8c87dd61acf
Weslley Prado <weslley.prado73@gmail.com>
2022-03-21 09:45:33 -0300

    Create Menu

--- Projects React Native/replica_coursify.me/coursifyme/Screen/Principal.js
+++ Projects React Native/replica_coursify.me/coursifyme/Screen/Principal.js
@@ -1,11 +1,32 @@
+import { Image } from "@react-native-elements/themed";
 import React from "react";
-import { View, Text } from "react-native";
+import { View,  SafeAreaView, StyleSheet } from "react-native";
 
 
 export default function Principal(){
     return(
+        <SafeAreaView>
         <View>
-            <Text> Entrou</Text>
+            <Image
+            source={require('../assets/logo.png')}
+            style={{width:170, height:130}}
+            resizeMode="contain"
+             />
         </View>
+        </SafeAreaView>
+       
     )
 }
+
+const styles = StyleSheet.create({
+    header:{
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        paddingRight: 10,
+        paddingLeft: 10,
+        borderBottomWidth:2,
+        borderBottomColor:'#FFF'
+
+    }
+})
