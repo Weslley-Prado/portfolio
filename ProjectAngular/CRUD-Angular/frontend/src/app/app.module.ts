7fe52391acbe64f8f7101488f08153c19c337b0f
Weslley Prado <weslley.prado73@gmail.com>
2022-04-08 10:19:08 -0300

    Complete the function update and delete

--- ProjectAngular/CRUD-Angular/frontend/src/app/app.module.ts
+++ ProjectAngular/CRUD-Angular/frontend/src/app/app.module.ts
@@ -28,6 +28,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort'
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from  '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -42,7 +44,9 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    Read2Component
+    Read2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
